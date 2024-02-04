44c7e7cbe9abacd24beec42daf5095618be9a78d
Artemii <artemiithefrog95@icloud.com>
2024-02-04 14:32:41 +0100

    Initial Commit

---
+++ SwiftUIMovieDB/SwiftUIMovieDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMovieDBApp.swift
+//  SwiftUIMovieDB
+//
+//  Created by artemiithefrog . on 04.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMovieDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
